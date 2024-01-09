7d5e869a8e9dc1762857671fe5021393649c8636
rikvik2006 <75540124+rikvik2006@users.noreply.github.com>
2024-01-09 16:18:23 +0100

    feat: Added start via tmux

--- multiInstanceRunner.py
+++ multiInstanceRunner.py
@@ -1,10 +1,12 @@
 import json
 import time
 import subprocess
+import requests
+import os
 
 
 def modify_config(guild_id, channel_id):
-    # Modifica il file config.json
+    # Modify the config.json file
     config_path = "./data/config.json"
     with open(config_path, "r") as config_file:
         config_data = json.load(config_file)
@@ -16,7 +18,7 @@ def modify_config(guild_id, channel_id):
 
 
 def update_tokens(token):
-    # Aggiungi il token all'array nel file tokens.json
+    # Add the token to the tokens.json file
     tokens_path = "./data/tokens.json"
     with open(tokens_path, "r") as tokens_file:
         tokens_data = json.load(tokens_file)
@@ -29,8 +31,13 @@ def update_tokens(token):
 
 def main():
     processes = []
-    # Leggi il file instances_data.txt
+    runner_config = "runner_config.json"
     instances_file = "instances_data.txt"
+    with open(f"./data/{runner_config}", "r") as file:
+        runner_data = json.load(file)
+        use_tmux = runner_data["use_tmux"]
+
+    # Get the tokens from instances_data.txt
     with open(f"./data/{instances_file}", "r") as file:
         lines = file.readlines()
         for line in lines:
@@ -38,15 +45,38 @@ def main():
             token, guild_id, channel_id = data
             modify_config(guild_id, channel_id)
             update_tokens(token)
-            # Avvia lo script main.py
-            process = subprocess.Popen(["python", "main.py"])
-            processes.append(process)
-            print(f"Avviato processo con token {token}")
-            time.sleep(1)
 
-    print(
-        f"⭐ Tutti i processi sono stati avviati per un totale di {len(processes)} processi ⭐"
-    )
+            # Get the discord username
+            response = requests.get(
+                "https://discord.com/api/users/@me",
+                headers={"authorization": token},
+            )
+            discord_username = response.json()["username"]
+
+            # Start main.py
+            if use_tmux:
+                os.system("mkdir -p ./logs/")
+
+                process = subprocess.Popen(
+                    [
+                        "tmux",
+                        "new",
+                        "-d",
+                        "-s",
+                        f"mass-dm-{discord_username}",
+                        "'exec python3 main.py 2>&1 | tee -a ./logs/{discord_username}.log'",
+                    ]
+                )
+                processes.append(process)
+                print(f"Process started: Username: {discord_username}, Token: {token}")
+                time.sleep(1)
+            else:
+                process = subprocess.Popen(["python", "main.py"])
+                processes.append(process)
+                print(f"Process started: Username: {discord_username}, Token: {token}")
+                time.sleep(1)
+
+    print(f"⭐ All processes were started for a total of {len(processes)} processes ⭐")
 
 
 if __name__ == "__main__":
